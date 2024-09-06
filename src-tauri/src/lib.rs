e9ac475cd7e7ecdca9afda1b1048a6ad2a10a1b1
Timur Carpeev <timuric@users.noreply.github.com>
2024-09-06 11:39:23 +0200

    Initial

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,25 @@
+#[cfg(desktop)]
+mod tray;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            #[cfg(all(desktop))]
+            {
+                let handle = app.handle();
+                tray::create_tray(handle)?;
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
